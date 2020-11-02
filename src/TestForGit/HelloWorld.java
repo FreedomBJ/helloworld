3ea822b2f8c00300f99f6c245cb481a9ed7b6060
FreedomBJ <zhouquan79@hotmail.com>
2020-11-02 16:50:14 +0800

    Master branch for git test.

---
+++ src/TestForGit/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package TestForGit;
+
+/**
+ * @author Quan
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
